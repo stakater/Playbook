58f351b81e965c808776045de5740fcbd3c4e32e
irti <aliartiza75@yahoo.com>
2020-01-23 11:41:48 +0500

    [update-playbook] update config.js and remove chartmuseum manifest

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -156,68 +156,6 @@ module.exports = {
                     '/content/processes/monitoring/creating-hooks-slack'
                 ]
             },
-            // {
-            //     title: 'Tools: Global Stack',
-            //     children: [
-            //         '/content/tools/global/external-dns/developer-documentation',
-            //         '/content/tools/global/gitwebhookproxy/developer-documentation',
-            //         '/content/tools/global/keycloak/developer-documentation',
-            //         '/content/tools/global/kubernetes-dashboard/developer-documentation',
-            //         '/content/tools/global/nginx-ingress/developer-documentation',
-            //         '/content/tools/global/configmap/developer-documentation',
-            //         '/content/tools/global/secrets/developer-documentation',
-            //         '/content/tools/global/storage/developer-documentation',
-            //         '/content/tools/global/rbac/developer-documentation',
-            //         '/content/tools/global/forecastle/developer-documentation',
-            //         '/content/tools/global/proxyinjector/developer-documentation'
-            //     ]
-            // },
-            // {
-            //     title: 'Tools: Logging Stack',
-            //     children: [
-            //         '/content/tools/logging/cerebro/developer-documentation',
-            //         '/content/tools/logging/elasticsearch/developer-documentation',
-            //         '/content/tools/logging/elasticsearch-curator/developer-documentation',
-            //         '/content/tools/logging/elasticsearch-operator/developer-documentation',
-            //         '/content/tools/logging/fluentd/developer-documentation',
-            //         '/content/tools/logging/kibana/developer-documentation',
-            //         '/content/tools/logging/konfigurator/developer-documentation',
-            //         ['/content/tools/logging/konfigurator/fluentd','Konfigurator with Fluentd'],
-            //         '/content/tools/logging/logrotate/developer-documentation'
-            //     ]
-            // },
-            // {
-            //     title: 'Tools: Release Stack',
-            //     children: [
-            //         '/content/tools/release/chartmuseum/developer-documentation',
-            //         '/content/tools/release/jenkins/developer-documentation',
-            //         '/content/tools/release/nexus/developer-documentation',
-            //         '/content/tools/release/restful-distributed-lock-manager/developer-documentation',
-            //         '/content/tools/release/sonarqube/developer-documentation'
-            //     ]
-            // },
-            // {
-            //     title: 'Tools: Monitoring Stack',
-            //     children: [
-            //         '/content/tools/monitoring/prometheus-operator/developer-documentation',
-            //         '/content/tools/monitoring/prometheus/developer-documentation',
-            //         '/content/tools/monitoring/grafana/developer-documentation',
-            //         '/content/tools/monitoring/kube-state-metrics/developer-documentation',
-            //         '/content/tools/monitoring/node-exporter/developer-documentation',
-            //         '/content/tools/monitoring/alert-manager/developer-documentation'
-            //     ]
-            // },
-            // {
-            //     title: 'Tools: Tracing Stack',
-            //     children: [
-            //         '/content/tools/tracing/istio/developer-documentation',
-            //         '/content/tools/tracing/envoy/developer-documentation',
-            //         '/content/tools/tracing/pilot/developer-documentation',
-            //         '/content/tools/tracing/mixer/developer-documentation',
-            //         '/content/tools/tracing/citadel/developer-documentation',
-            //         '/content/tools/tracing/jaeger/developer-documentation'
-            //     ]
-            // },
             {
                 title: 'Workshop: Setting Up',
                 children: [
